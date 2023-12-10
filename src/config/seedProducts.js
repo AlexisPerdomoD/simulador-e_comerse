fae3148a2bafbab9d430070d0080a14f9ad541b6
AlexisPerdomoD <dropdibemol@gmail.com>
2023-12-10 12:20:05 -0500

    solucionar issues firebase

--- src/config/seedProducts.js
+++ src/config/seedProducts.js
@@ -1,10 +1,11 @@
 import { addDoc, collection } from "firebase/firestore";
 import { destructurador } from "../assets/destructurador"
 import { db } from "./fireBaseConfig";
+import info from "../info.json"
 
 
-export const seedProducts = (productos) => {
-    let products = destructurador(productos);
+export const seedProducts = () => {
+    let products = destructurador(info);
     products.forEach(product =>{
         addDoc(collection(db, "products"), product)
     })
